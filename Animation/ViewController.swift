18cb96b9c871cbfd87bfbbb1388fb0e749d32dbd
anthrgrnwrld <anthrgrnwrld@gmail.com>
2015-12-20 09:30:38 +0900

    1st commit

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var positionViewLeft: UIView!
+    @IBOutlet weak var positionViewRight: UIView!
+    @IBOutlet weak var orangeColorView: UIView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var animationFlag = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +25,58 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print("\(__FUNCTION__) is called!!")
+        
+        /**
+        アニメーション移動後のViewの座標を設定します。
+        アニメション後の座標の管理方法はアプリによって異なると思われます。
+        */
+
+        //アニメーションするか否か確認。アニメーションしない場合にはreturnする。
+        if animationFlag == false {
+            return
+        }
+        
+        orangeColorView.center = positionViewRight.center   //移動後のorangeColorViewの座標を設定
+        animationFlag = false                               //アニメーションフラグをfalseに
+        
+        
+    }
+
+    /**
+     ボタンを押した時の動作。アニメーション実行のトリガ
+     */
+    @IBAction func pressButton(sender: AnyObject) {
+        
+        print("\(__FUNCTION__) is called!!")
+        
+        animationFlag = true            //アニメーションフラグをtrueにする
+        
+        self.textLabel.text = "After"   //テキストをafterへ変更 (←これがアニメーションが意図しない動きになる原因！！)
+ 
+        animationLocateWithView(orangeColorView, targetPoint: positionViewRight.center) //アニメーション実行
+        
+    }
+    
+    /**
+     指定されたViewを指定された中心座標へアニメーションする
+     
+     - parameter targetView:アニメーションさせるView
+     - parameter targetPoint:異動先の座標
+     */
+    func animationLocateWithView(targetView :UIView, targetPoint :CGPoint) {
+        
+        print("\(__FUNCTION__) is called!!")
+        
+        let animationSpeed = 1.0
+        
+        UIView.animateWithDuration(animationSpeed, animations: { () -> Void in
+
+            targetView.center = targetPoint
+            }, completion: nil)
     }
 
 
